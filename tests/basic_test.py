ce4579b0bcc399dffa8c866d9e6672d41ba411ec
Kamil Kruk <Kamil.Kruk@jeppesen.com>
2020-02-29 15:23:29 +0100

    rearrange files structure, added test template

---
+++ tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2019
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
